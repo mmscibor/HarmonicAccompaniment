e390f7c14b9a2eb3a15b2a6744d58e53250d4b49
mmscibor <mmscibor@gmail.com>
2014-02-11 15:02:33 -0500

    Include library for HMM

--- src/BaseClass.java
+++ src/BaseClass.java
@@ -1,11 +1,22 @@
+import be.ac.ulg.montefiore.run.jahmm.Hmm;
+import be.ac.ulg.montefiore.run.jahmm.ObservationDiscrete;
+import be.ac.ulg.montefiore.run.jahmm.OpdfDiscreteFactory;
+
 import java.io.IOException;
+import java.util.List;
 
 public class BaseClass {
 
-    public static void main(String[] args){
-//        Input input = new Input();
-//        pauseForInput(input);
+    public enum State {
+        C, Csh, D, Dsh, E, F, Fsh, G, Gsh, A, Ash, B;
 
+    }
+
+    public static void main(String[] args){
+        Input input = new Input();
+        pauseForInput(input);
+//        List<ObservationDiscrete<State>> sequence;
+//        sequence.add()
 
     }
 
